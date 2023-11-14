cbe4ed9062a02094353bb5b5fd4394c9ebb322da
Dogolaa <lucasdogo01@gmail.com>
2023-11-14 19:06:10 -0300

    Configurando BackEnd & mysql

--- petshop/server/dbServices.js
+++ petshop/server/dbServices.js
@@ -1,19 +1,45 @@
-const mysql = require('mysql2')
-const dotenv = require('dotenv')
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
 
-dotenv.config()
+dotenv.config();
 
 const connection = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.DB_USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    port: process.env.PORT
+  host: process.env.HOST,
+  user: process.env.DB_USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.DB_PORT,
 });
 
-connection.connect((err)=>{
-    if(err){
-        console.log(err);
+connection.connect((err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log("BD foi conectado com sucesso!");
+});
+
+class dbServices {
+  static instance;
+
+  static getdbServicesInstance() {
+    if (!this.instance) {
+      this.instance = new dbServices();
     }
-    console.log("BD foi conectado com sucesso!")
-})
+    return this.instance;
+  }
+
+  async BuscarClientes() {
+    return new Promise((resolve, reject) => {
+      const query = "SELECT * FROM tbl_clientes";
+      connection.query(query, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
+}
+
+module.exports = dbServices;
--- petshop/server/index.js
+++ petshop/server/index.js
@@ -1,11 +1,27 @@
 const express = require('express')
 const app = express();
 const cors = require('cors');
-const dbServices. = require('./dbServices.js')
+const dbServices = require('./dbServices.js')
+
+app.use(cors())
+app.use(express.json())
+
+
+const db = dbServices.getdbServicesInstance();
+
 
 app.get('/', (request, response) => {
    response.send("Hello, World!") 
 });
+
+
+app.get('/BuscarClientes', (request, response) => {
+    const result = db.BuscarClientes();
+    result
+        .then(data=> response.json(data))
+        .catch(err => console.log(err))
+ });
+
 
 app.get('/teste', (request, response) => {
     response.send("EndPoint de teste!")
